b98afe0799a05e7665556863d1f77afdc4243d7a
Madhavi <madhavinagapurkar@gmail.com>
2021-01-11 13:34:25 +0530

    adding angular project to github

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { UserRegisterComponent } from './user-register/user-register.component';
+import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
+import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
+import { UserTestComponent } from './user-test/user-test.component';
+import { UserReportComponent } from './user-report/user-report.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    UserRegisterComponent,
+    UserDashboardComponent,
+    AdminDashboardComponent,
+    UserTestComponent,
+    UserReportComponent
   ],
   imports: [
     BrowserModule,
