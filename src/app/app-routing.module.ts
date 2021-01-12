579b1fe883430d413648e44c77840310e3a32197
sahithi2604 <37655909+sahithi2604@users.noreply.github.com>
2021-01-12 12:33:59 +0530

    template

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,13 @@ import { UserDashboardComponent } from './user-dashboard/user-dashboard.componen
 import { TestSubjectComponent } from './test-subject/test-subject.component';
 import { UserReportComponent } from './user-report/user-report.component';
 import { UserTestComponent } from './user-test/user-test.component';
+import { UserRegisterComponent } from './user-register/user-register.component';
 const routes: Routes = [{path: 'take-test',component:UserTestComponent},
   { path: 'dashboard', component: UserDashboardComponent },
   { path: 'login', component: LoginComponent },
+  {path: 'register', component: UserRegisterComponent},
   {path:'view-report',component:UserReportComponent},
-{path:'view-subject',component:TestSubjectComponent}
+  {path:'view-subject',component:TestSubjectComponent}
 ];
 
 
