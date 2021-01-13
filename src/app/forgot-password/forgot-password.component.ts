3d47319d275584e4a21c8d63e19480315b3060cc
Ashish <ashishmattam999@gmail.com>
2021-01-13 09:56:59 +0530

    AdminLogin

--- src/app/forgot-password/forgot-password.component.ts
+++ src/app/forgot-password/forgot-password.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { LoginService } from '../login.service';
 
 @Component({
   selector: 'app-forgot-password',
   templateUrl: './forgot-password.component.html',
   styleUrls: ['./forgot-password.component.css']
 })
-export class ForgotPasswordComponent implements OnInit {
+export class ForgotPasswordComponent{
+    ForgotPasswordForm:FormGroup;
+    message:string;
+    IsvalidForm=true;
+    constructor(private loginService:LoginService,private router:Router){}
 
-  constructor() { }
+    ngOnInIt(){
+      this.ForgotPasswordForm= new FormGroup({
+        'email':new FormControl(null,[Validators.required,Validators.email]),
+      });
+    }
 
-  ngOnInit(): void {
+}
+
+ForgotPassword(form){
+  console.log(form)
+  if(form.valid){
+    this.IsvalidForm=true;
+    this.studentService.forgotPassword(this.RequestResetForm.value).subscribe(
+      data=>{
+        this.RequestResetForm.reset();
+        this.message="Reset password link is shared to your email";
+      }
+    )
+
   }
+  else{
+    this.IsvalidForm=false;
+  }
+}
 
 }
